124c4cb93fe2791130d71ce29842355b13d0ae45
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-03 18:40:11 +0530

    [sailesh] Add : Implemented UC3

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace PersonDataManagement
             List<Person> personList = new List<Person>();
             addPersonData_In_personList(personList);
             Retrieving_TopTwoRecord_ForAgeIs_LessThanSixty(personList);
+            Retrieving_AllRecords_Btw_Age13_To_18(personList);
 
         }
         public static void addPersonData_In_personList(List<Person> personList)
@@ -18,7 +19,7 @@ namespace PersonDataManagement
             personList.Add(new Person("4578913", "Barack", "Washington", 65));
             personList.Add(new Person("4561333","Donald","NewYork",62));
             personList.Add(new Person("4567895","Sammy","Albama",17));
-            personList.Add(new Person("4598962", "Samuel", "NewJersey", 45));
+            personList.Add(new Person("4598962", "Samuel", "NewJersey", 14));
             personList.Add(new Person("203456879", "Smith", "12 Main Street, Newyork,NY", 45));
             personList.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton,OH", 55));
             personList.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", 65));
@@ -31,6 +32,13 @@ namespace PersonDataManagement
                 Console.WriteLine("Name: "+person.Name+"\t\tAge: "+person.Age);
             }
         }
-
+        public static void Retrieving_AllRecords_Btw_Age13_To_18(List<Person> personList)
+        {
+            Console.WriteLine("Details of Person Between age of 13 to 18");
+            foreach (Person person in personList.FindAll(e=>(e.Age>=13 && e.Age <= 18)).ToList())
+            {
+                Console.WriteLine("Name: "+person.Name+"\t\tAge:"+person.Age);
+            }
+        }
     }
 }
