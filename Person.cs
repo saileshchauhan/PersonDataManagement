b78397c4d95c49856584ff16a11c578a8fe89791
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-03 17:50:56 +0530

    [sailesh] Add: Data List Creation

---
+++ Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDataManagement
+{
+    class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Adress;
+        public int Age;
+
+        public Person(string ssn,string name,string adress,int age)
+        {
+            this.SSN = ssn;
+            this.Name = name;
+            this.Adress = adress;
+            this.Age = age;
+        }
+    }
+}
