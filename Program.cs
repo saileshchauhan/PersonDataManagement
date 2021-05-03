8edda12fe1884a15385c4193b4a47335ee6defff
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-03 18:18:39 +0530

    [sailesh] Add: UC 2 implementation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PersonDataManagement
 {
@@ -8,9 +9,11 @@ namespace PersonDataManagement
         static void Main(string[] args)
         {
             List<Person> personList = new List<Person>();
+            addPersonData_In_personList(personList);
+            Retrieving_TopTwoRecord_ForAgeIs_LessThanSixty(personList);
 
         }
-        public void addPersonData_In_personList(List<Person> personList)
+        public static void addPersonData_In_personList(List<Person> personList)
         {
             personList.Add(new Person("4578913", "Barack", "Washington", 65));
             personList.Add(new Person("4561333","Donald","NewYork",62));
@@ -20,6 +23,13 @@ namespace PersonDataManagement
             personList.Add(new Person("203456880", "SAM", "345 Main Ave, Dayton,OH", 55));
             personList.Add(new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", 65));
             personList.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
+        }
+        public static void Retrieving_TopTwoRecord_ForAgeIs_LessThanSixty(List<Person> personList)
+        {
+            foreach(Person person in personList.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name: "+person.Name+"\t\tAge: "+person.Age);
+            }
         }
 
     }
